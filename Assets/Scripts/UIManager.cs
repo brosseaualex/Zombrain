e65d44eca913915e59d0490a8af9f61ed6d9aba9
Alexandre Brosseau <brosseaualex@gmail.com>
2020-12-13 18:53:17 -0500

    Asset dump to push on GitHub

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,67 @@
+﻿using Destructible2D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager
+{
+    #region Singleton
+    private UIManager() { }
+    private static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new UIManager();
+            return instance;
+        }
+    }
+    #endregion
+
+    Text humansCount;
+    Text zombiesCount;
+
+    GameObject fishHudPanel;
+    GameObject fishAmmoPanelPrefab;
+    GameObject fishAmmoPanelClone;
+    List<GameObject> ammoList;
+
+    public void Initialize()
+    {
+        humansCount = GameObject.Find("HumansText").GetComponent<Text>();
+        zombiesCount = GameObject.Find("ZombiesText").GetComponent<Text>();
+        fishHudPanel = GameObject.Find("FishHUDPanel");
+        fishAmmoPanelPrefab = Resources.Load<GameObject>("Prefabs/UI/FishAmmoPanel");
+        ammoList = new List<GameObject>();
+    }
+
+    public void Update()
+    {
+        humansCount.text = "Humans: " + UnitManager.Instance.humans.Count;
+        zombiesCount.text = "Zombies: " + UnitManager.Instance.zombies.Count;
+    }
+
+    public void AddAmmo()
+    {
+        fishAmmoPanelClone = new GameObject();
+        fishAmmoPanelClone = GameObject.Instantiate(fishAmmoPanelPrefab);
+        fishAmmoPanelClone.transform.SetParent(fishHudPanel.transform, false);
+        //ammoList.Add(fishAmmoPanelClone);
+
+    }
+
+    public void RemoveAmmoUI()
+    {
+        ammoList.Remove(fishAmmoPanelClone);
+        //GameObject.Destroy(fishAmmoPanelClone);
+    }
+
+    public void RefreshUI(int ammo)
+    {
+  
+
+    }
+
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Zombrain
+
+## Description
+
+This project was an exam/test I had to go through during my Unity game developement course at school.
+
+## Important Notice
+
+For this test, I was provided with a small/medium sized project with some bugs and missing features and I had to find those bugs and add the missing feature(s).
+
+This project was also used to "test" my analytical skill and there was a series of questions asked referring to certain portion of the code and I had to research/analyse the code and find the answers.
+
+The "complete" test questions can be found in the "Assets/Test.cs" file.
